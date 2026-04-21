7f470ba9f0253a77a2e91deeb2ec8e20b9021454
Sevatar88 <veselov5gorsk@mail.ru>
2026-04-21 16:09:21 +0300

    Update index.js

--- Homework19/js/index.js
+++ Homework19/js/index.js
@@ -20,7 +20,7 @@ try {
                 CLOSE: "Закрыть меню",
             },
             PAGE_BODY: "page__body",
-            PAGE_BODY_NO_SCROLL: "page__body--no--scroll",
+            PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
             MENU_LINK: "menu__link",
             BREAKPOINT: 768,
             MAIN: "main",
