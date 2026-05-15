034ac00ee34ab132726b5c4c43e23971338e9a2c
Sevatar88 <veselov5gorsk@mail.ru>
2026-05-15 22:59:10 +0300

    Update script.js

--- Homework21/script.js
+++ Homework21/script.js
@@ -9,12 +9,22 @@ console.log(isStudent);
 const age = `38`;
 const currentYear = `2026`;
 const birthYear = currentYear - age;
-const message = `Меня зовут `  + firstName + lastName + `,` + `мне ` + age + ` лет` + `.` + `Я ученик курса: ` + isStudent
+
+const aa = `Меня зовут `;
+const bb = firstName;
+const cc = lastName;
+const dd = `, мне `;
+const ee = age;
+const f = ` лет.`;
+const g = `Я ученик курса: `;
+const h = isStudent;
+const Result = aa + bb + cc + dd + ee + f + g + h;
+
+console.log(Result);
 
 console.log(age);
 console.log(currentYear);
 console.log(birthYear);
-console.log(message);
 
 let a = `123`;
 let b = +`456`;
