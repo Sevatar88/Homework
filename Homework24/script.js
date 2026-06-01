2917f4dcacd9835e0a6514b69c701d7b4adfa0f9
Sevatar88 <veselov5gorsk@mail.ru>
2026-06-01 14:28:13 +0300

    Add files via upload

---
+++ Homework24/script.js
@@ -0,0 +1,61 @@
+//1 
+
+function calculateFinalPrice(basePrice, discountPercent, taxRate) {
+    const discount = (basePrice * discountPercent) / 100;
+    const taxPrice = (basePrice * taxRate) / 100;
+    const price = basePrice - discount + taxPrice;
+    // const price = basePrice - ((basePrice * discountPercent) / 100) + ((basePrice * taxRate) / 100);
+    return price;
+}
+
+const basePrice = +prompt("Введите базовую стоимость в рублях");
+const discountPercent = +prompt("Введите процент скидки в %");
+const taxRate = +prompt("Введите налоговую ставку в %");
+console.log(calculateFinalPrice(basePrice, discountPercent, taxRate));
+
+
+//2 
+
+function checkAccess(login, password) {
+    if ((login === "admin" || login === "Admin") && password === "123456") {
+        return "Доступ разрешён";
+    } else {
+        return "Доступ запрещён";
+    }
+}
+
+const login = prompt("Введите имя пользователя");
+const password = prompt("Введите пароль");
+console.log(checkAccess(login, password));
+
+//3 
+
+function getTimeOfDay(number) {
+    if (number >= 0 && number <= 5) {
+        return "Ночь";
+    } else if (number >= 6 && number <= 11) {
+        return "Утро";
+    } else if (number >= 12 && number <= 17) {
+        return "День";
+    } else if (number >= 18 && number <= 23) {
+        return "Вечер";
+    } else {
+        return "Некорректное время";
+    }
+}
+
+const number = +prompt("Введите текущее время");
+console.log(getTimeOfDay(number));
+
+//4 
+
+function findFirstEven(start, end) {
+    for (let i = start; i <= end; i++) {
+        if (i % 2 === 0) return i;
+    }
+    return "Нет чётных чисел";
+}
+
+const start = +prompt("Введите первое число");
+const end = +prompt("Введите второе число");
+console.log(findFirstEven(start, end));
