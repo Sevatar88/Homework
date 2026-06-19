283446a97963a64c6a970dee473879b32bb6603c
Sevatar88 <veselov5gorsk@mail.ru>
2026-06-19 17:25:15 +0300

    Add files via upload

---
+++ Homework27/js/main.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const taskKeys = {
+    id: "id",
+    text: "text",
+    done: "done",
+};
+
+const dailyTasks = [];
+
+const dailyTaskError = (taskId) => {
+    return `There is no task with id: ${taskId}`;
+};
+
+const newDailyTaskId = (dailyTasks) =>
+    dailyTasks.reduce((maxId, task) => Math.max(maxId, task[taskKeys.id]), 0) +
+    1;
+
+const createNewDailyTask = (dailyTasks, text) => {
+    const newTask = {
+        [taskKeys.id]: newDailyTaskId(dailyTasks),
+        [taskKeys.text]: text,
+        [taskKeys.done]: false,
+    };
+    dailyTasks.push(newTask);
+    return newTask;
+};
+
+
+const editDailyTask = (dailyTasks, taskId, text) => {
+    const id = dailyTasks.find((task) => task[taskKeys.id] === taskId);
+    if (id !== -1) {
+        dailyTasks[id] = `Отредактирована задача № ${taskId}`;
+    }
+        const editTask = { [taskKeys.id]: newDailyTaskId(dailyTasks), [taskKeys.text]: text, [taskKeys.done]: false, };
+    dailyTasks.splice(newDailyTaskId, 1);
+    return editTask;
+};
+
+const completeDailyTaskById = (dailyTasks, taskId) => {
+    const task = dailyTasks.find((task) => task[taskKeys.id] === taskId);
+    if (!task) {
+        console.error(errTaskNotFound(taskId));
+        return null;
+    }
+    task[taskKeys.done] = !task[taskKeys.done];
+    return task;
+};
+
+const deleteDailyTaskById = (dailyTasks, taskId) => {
+    const taskIndex = dailyTasks.findIndex(
+        (task) => task[taskKeys.id] === taskId
+    );
+    if (taskIndex === -1) {
+        console.error(errTaskNotFound(taskId));
+        return dailyTasks;
+    }
+    dailyTasks.splice(taskIndex, 1);
+    return dailyTasks;
+};
